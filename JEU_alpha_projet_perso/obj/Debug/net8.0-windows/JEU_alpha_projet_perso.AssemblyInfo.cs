390ef200425bc6b48ebabbef4f7a5da1a3284b75
Maxime Frossard <maxfrossard@etmlnet.local>
2025-05-05 14:59:54 +0200

    aze

--- JEU_alpha_projet_perso/obj/Debug/net8.0-windows/JEU_alpha_projet_perso.AssemblyInfo.cs
+++ JEU_alpha_projet_perso/obj/Debug/net8.0-windows/JEU_alpha_projet_perso.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JEU_alpha_projet_perso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315c4858b1f3588d95b163cb2b6adc30890fc43a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb9f7dbd1c535c141e046d8975ef4c7fe6820294")]
 [assembly: System.Reflection.AssemblyProductAttribute("JEU_alpha_projet_perso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JEU_alpha_projet_perso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
